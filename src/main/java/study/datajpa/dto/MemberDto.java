3d6a54a992d08df2650bf4542cf1fdc58e11fe40
jang010505 <jang010505@inu.ac.kr>
2023-07-30 02:55:55 +0900

    feat: upload session 6

--- src/main/java/study/datajpa/dto/MemberDto.java
+++ src/main/java/study/datajpa/dto/MemberDto.java
@@ -1,9 +1,11 @@
 package study.datajpa.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import study.datajpa.entity.Member;
 
 @Data
+@AllArgsConstructor
 public class MemberDto {
     private Long id;
     private String username;
--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
